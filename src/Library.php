f10f3d4211bca11616f60aa0fc210823b6037c4d
twosee <twose@qq.com>
2024-05-13 02:45:46 +0800

    Release v1.5.1

--- src/Library.php
+++ src/Library.php
@@ -15,12 +15,12 @@ namespace Swow;
 
 final class Library
 {
-    public const VERSION = '1.5.1-dev';
+    public const VERSION = '1.5.1';
     public const VERSION_ID = 10501;
     public const MAJOR_VERSION = 1;
     public const MINOR_VERSION = 5;
     public const RELEASE_VERSION = 1;
-    public const EXTRA_VERSION = 'dev';
+    public const EXTRA_VERSION = '';
 
-    public const REQUIRED_EXTENSION_VERSION = '^1.5.0';
+    public const REQUIRED_EXTENSION_VERSION = '^1.5.1';
 }
